1a5175d8ce001720184b94222d25a883d9c1bbc5
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-18 19:00:30 +0330

    polish codebase to implement LsTree functionality

--- src/commands/ls_tree.rs
+++ src/commands/ls_tree.rs
@@ -1,16 +1,43 @@
+use crate::objects::{Kind, Object};
 use anyhow::Context;
-use flate2::read::ZlibDecoder;
-use std::ffi::CStr;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+use std::{ffi::CStr, io::BufRead};
 
-enum Kind {
-    Blob,
-}
+pub(crate) fn invoke(name_only: bool, tree_hash: &str) -> anyhow::Result<()> {
+    anyhow::ensure!(name_only, "mode or -p must be given.");
 
-pub(crate) fn invoke(name_only: bool) -> anyhow::Result<()> {
-    anyhow::ensure!(name_only, "only --name-only is supproted for now");
-    todo!();
+    let mut object = Object::read(tree_hash).context("parse out tree file")?;
+    // TODO: support shortest-unique object hashes
+    match object.kind {
+        Kind::Tree => {
+            let mut buf = Vec::new();
+            loop {
+                buf.clear();
+                let n = object
+                    .reader
+                    .read_until(0, &mut buf)
+                    .context("read next tree object entry")?;
+                if n == 0 {
+                    break;
+                }
+                let mode_and_name =
+                    CStr::from_bytes_with_nul(&buf).context("invalid tree entry")?;
+                let mut bits = mode_and_name.to_bytes().splitn(2, |&b| b == b' ');
+                let mode = bits.next().expect("split always yield once.");
+                let name = bits
+                    .next()
+                    .ok_or_else(|| anyhow::anyhow!("tree entry has no filename"))?;
+            }
+            let stdout = std::io::stdout();
+            let mut stdout = stdout.lock();
+            let n = std::io::copy(&mut object.reader, &mut stdout)
+                .context("write .git/objects file to the stdout")?;
+            anyhow::ensure!(
+                n == object.expected_size,
+                ".git/objects file was not the expected size: (expected: {}, actual: {n})",
+                object.expected_size
+            );
+        }
+        _ => anyhow::bail!("don't yet know how to ls '{}'", object.kind),
+    }
     Ok(())
 }
