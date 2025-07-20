9ea9d1aa84af92894dd90b4592eb6be038c2ee1d
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-20 22:41:28 +0330

    polished based on Object itself

--- src/commands/write_tree.rs
+++ src/commands/write_tree.rs
@@ -1,8 +1,8 @@
 use anyhow::Context;
-use std::fs;
-use std::io::Cursor;
+use hex::encode;
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
+use std::{fs, io::Cursor};
 
 use crate::objects::{Kind, Object};
 
@@ -13,6 +13,10 @@ pub(crate) fn write_tree_for(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
     while let Some(entry) = dir.next() {
         let entry = entry.with_context(|| format!("bad directory {}", path.display()))?;
         let file_name = entry.file_name();
+        if file_name == ".git" {
+            // skip the .git directory
+            continue;
+        }
         let meta = entry.metadata().context("metadata for directory entry")?;
         let mode = if meta.is_dir() {
             "40000"
@@ -58,23 +62,22 @@ pub(crate) fn write_tree_for(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
     if tree_object.is_empty() {
         Ok(None)
     } else {
-        let temp = "temporary";
-        let hash = Object {
-            kind: Kind::Tree,
-            expected_size: tree_object.len() as u64,
-            reader: Cursor::new(tree_object),
-        }
-        .write(std::fs::File::create(temp).context("construct temporary file for blob.")?)
-        .context("stream file into blob")?;
-        let hash_hex = hex::encode(hash);
-        std::fs::create_dir_all(format!(".git/objects/{}/", &hash_hex[..2]))
-            .context("create subdir of .git/objects")?;
-
-        std::fs::rename(
-            temp,
-            format!(".git/objects/{}/{}", &hash_hex[..2], &hash_hex[..2]),
-        )
-        .context("moved blob file into .git/objects")?;
-        Ok(Some(hash))
+        Ok(Some(
+            Object {
+                kind: Kind::Tree,
+                expected_size: tree_object.len() as u64,
+                reader: Cursor::new(tree_object),
+            }
+            .write_to_objects()
+            .context("write tree object")?,
+        ))
     }
+}
+
+pub(crate) fn invoke() -> anyhow::Result<()> {
+    let Some(hash) = write_tree_for(Path::new(".")).context("construct root tree object")? else {
+        anyhow::bail!("asked to make tree object for empty tree.");
+    }
+    println!("{}", encode(hash));
+    Ok(())
 }
