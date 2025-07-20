89630630e5dfb051e59857459589aa6934f15862
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-20 22:40:51 +0330

    pushed all Object specific implementation to Object itself

--- src/objects.rs
+++ src/objects.rs
@@ -1,10 +1,16 @@
 use anyhow::Context;
 use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use sha1::{Digest, Sha1};
 use std::ffi::CStr;
 use std::fs::File;
+use std::io::copy;
 use std::io::prelude::*;
 use std::io::BufReader;
-
+use std::io::Cursor;
+use std::io::Write;
+use std::path::Path;
 #[derive(Debug, PartialEq, Eq)]
 pub(crate) enum Kind {
     Blob,
@@ -29,6 +35,17 @@ pub(crate) struct Object<R> {
 }
 
 impl Object<()> {
+    pub(crate) fn blob_from_file(file: impl AsRef<Path>) -> anyhow::Result<Object<impl Read>> {
+        let file = file.as_ref();
+        let stat = std::fs::metadata(file).with_context(|| format!("stat {}", file.display()))?;
+        let file = std::fs::File::open(file).context("open .git/objects file")?;
+        Ok(Object {
+            kind: Kind::Blob,
+            expected_size: stat.len(),
+            reader: file,
+        })
+    }
+
     pub(crate) fn read(hash: &str) -> anyhow::Result<Object<impl BufRead>> {
         let f = File::open(format!(".git/objects/{}/{}", &hash[..2], &hash[2..]))
             .context("open in .git/objects")?;
@@ -60,5 +77,61 @@ impl Object<()> {
             expected_size: size,
             reader: z,
         })
+    }
+}
+impl<R> Object<R>
+where
+    R: Read,
+{
+    pub(crate) fn write(mut self, writer: impl Write) -> anyhow::Result<[u8; 20]> {
+        // TODO: there is a race here if the file changes between stat and write.
+        let zlib_writer = ZlibEncoder::new(writer, Compression::default());
+        let mut writer = HashWriter {
+            writer: zlib_writer,
+            hasher: Sha1::new(),
+        };
+
+        write!(writer, "{} {}\0", self.kind, self.expected_size)?;
+        copy(&mut self.reader, &mut writer).context("Stream file into blob")?;
+        let _ = writer.writer.finish()?;
+        let hash = writer.hasher.finalize();
+        Ok(hash.into())
+    }
+
+    pub(crate) fn write_to_objects(self) -> anyhow::Result<[u8; 20]> {
+        let temp = "temporary";
+        let hash = self
+            .write(std::fs::File::create(temp).context("construct temporary file for tree.")?)
+            .context("stream tree object into tree object file")?;
+        let hash_hex = hex::encode(hash);
+        std::fs::create_dir_all(format!(".git/objects/{}/", &hash_hex[..2]))
+            .context("create subdir of .git/objects")?;
+
+        std::fs::rename(
+            temp,
+            format!(".git/objects/{}/{}", &hash_hex[..2], &hash_hex[..2]),
+        )
+        .context("moved tree file into .git/objects")?;
+        Ok(hash)
+    }
+}
+
+struct HashWriter<W> {
+    writer: W,
+    hasher: Sha1,
+}
+
+impl<W> Write for HashWriter<W>
+where
+    W: Write,
+{
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let n = self.writer.write(buf)?;
+        self.hasher.update(&buf[..n]);
+        Ok(n)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.writer.flush()
     }
 }
